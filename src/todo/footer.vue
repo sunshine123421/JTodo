a2ec1b4c7603941c0be7560c0608ea7950268f84
yangli <770310207@qq.com>
2018-04-30 12:05:17 +0800

    add code

---
+++ src/todo/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-footer">
+      <h6>Reality</h6>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+    .main-footer{
+        text-align center
+        h6{
+            font-size 10px
+        }
+    }
+
+
+</style>
